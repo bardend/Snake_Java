d859e08540f6b40664d22def50e6fed5b96723c8
Cody McCodePants <cody@mccodepants.com>
2022-08-05 12:17:30 -0500

    Food

--- src/test/Food.java
+++ src/test/Food.java
@@ -1,4 +1,28 @@
 package test;
 
+import java.awt.*;
+
 public class Food {
+    private int x;
+    private int y;
+    public Food(Snake player){
+        this.random_spawn(player);
+    }
+    public void random_spawn(Snake player){
+        boolean onSnake = true ;
+        while(onSnake){
+            x = (int)(Math.random()*Game.width);
+            y = (int)(Math.random()*Game.height);
+            boolean check = true ;
+           for(Rectangle r: player.getBody()){
+               if(r.x == x && r.y == y){
+                   check = false ;
+               }
+           }
+
+            if(check == true){
+                onSnake = false ;
+            }
+        }
+    }
 }
